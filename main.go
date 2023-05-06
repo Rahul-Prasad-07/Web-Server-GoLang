b0ef4e4eeb50e6ba8e6baa87bbc9a40ba8c23b59
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-05-06 15:50:34 +0530

    main.go: formHandler & helloHandler

--- main.go
+++ main.go
@@ -10,9 +10,36 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 
+	// if the path is not equal to /hello then it will return 404 not found
+	if r.URL.Path != "/hello" {
+
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+
+	}
+
+	// if the method is not equal to GET then it will return Method is not supported
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	// if the method is GET then it will return hello!
+	fmt.Fprintf(w, "hello !")
+
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err) // if there is any error then it will return error
+		return
+	}
+	fmt.Fprintf(w, "Post request succesfull")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 
 }
 
